a38f28544a82deb26b70e8ee5517d1545f138044
rakeshsmw <65362408+rakeshsmw@users.noreply.github.com>
2021-10-02 06:28:07 -0700

    Java Day1_Assignments

---
+++ Rakesh.java
@@ -0,0 +1,7 @@
+class Rakesh
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hi,I am Rakesh Kumar from Chennai ,garduated in B.E(CSE) and I generally like the Programming like c,Java,python and then I'm currently looking for job");
+    }
+}
